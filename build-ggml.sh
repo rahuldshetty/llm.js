fe533fe4981e816340db5751c39bb5b7a0b42537
Rahul <35rahuldshetty@gmail.com>
2023-08-05 23:22:10 +0530

    created git patch

--- build-ggml.sh
+++ build-ggml.sh
@@ -7,10 +7,17 @@ GGML_DIR=$BUILD_DIR/ggml
 
 TARGET_DIR=$BUILD_DIR/bin
 
+# Clone ggml library
 if [ ! -d $GGML_DIR ]; then
+    echo "Downloading GGML repository..."
     git clone https://github.com/ggerganov/ggml.git $GGML_DIR
     cd $GGML_DIR
     git reset --hard $GGML_HASH
+
+    # Apply patch
+    echo "Applying patch..."
+    git apply ../../patches/00-disable-hardwares.patch
+
     cd ../../ # move to root directory
 fi
 
@@ -25,5 +32,3 @@ cd $TARGET_DIR
 emcmake cmake ../../$GGML_DIR -DGGML_NO_ACCELERATE=ON
 
 make CC=emcc CXX=em++ CFLAGS="-DNDEBUG -s MEMORY64" CXXFLAGS="-DNDEBUG -s MEMORY64" LDFLAGS="-s MEMORY64 -s FORCE_FILESYSTEM=1 -s EXPORT_ES6=1 -s MODULARIZE=1 -s TOTAL_MEMORY=2GB -s STACK_SIZE=524288 -s ALLOW_MEMORY_GROWTH -s EXPORTED_FUNCTIONS=_main -s EXPORTED_RUNTIME_METHODS=callMain -s BUILD_AS_WORKER=1 -s SINGLE_FILE=1 -s NO_EXIT_RUNTIME=1"
-
-# make CC=emcc CXX=em++ CFLAGS=" -DNDEBUG -s MEMORY64" CXXFLAGS=" -DNDEBUG -s MEMORY64" LDFLAGS="-s MEMORY64 -s FORCE_FILESYSTEM=1 -s EXPORT_ES6=1 -s MODULARIZE=1 -s TOTAL_MEMORY=2GB -s STACK_SIZE=524288 -s ALLOW_MEMORY_GROWTH -s EXPORTED_FUNCTIONS=_main -s EXPORTED_RUNTIME_METHODS=callMain -s BUILD_AS_WORKER=1 -s SINGLE_FILE=1 -s NO_EXIT_RUNTIME=1"
